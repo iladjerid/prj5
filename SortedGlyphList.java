f56cf5d27231876c7d1fb4dc67aba52f44f6c703
iladjerid <iladjerid@gmail.com>
2017-04-19 19:23:54 -0400

    Update SortedGlyphList.java

--- SortedGlyphList.java
+++ SortedGlyphList.java
@@ -1,12 +1,19 @@
+
+/**
+* class used to store and order the glyphs created for each song so
+* so that they can be retrieved and called upon
+*/
 public class SortedGlyphList {
     private LList<Glyph> glyphList;
 
-
+    /**
+    * Constructor method used to initilaize the fields created
+    */
     public SortedGlyphList() {
         glyphList = new LList<Glyph>();
     }
 
-
+    
     public void add(SortedSongList songList, String command) {
         LList<Song> list = new LList<Song>();
         if (command == "name") {
