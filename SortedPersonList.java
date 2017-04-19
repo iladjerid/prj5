eacf40552141bfc0a1fec64aa38ada760cae75e0
kayley25 <kayley25@vt.edu>
2017-04-18 20:09:59 -0400

    Create SortedPersonList.java

---
+++ SortedPersonList.java
@@ -0,0 +1,27 @@
+public class SortedPersonList {
+    private LList<Person> hobbyList;
+    private LList<Person> regionList;
+    private LList<Person> majorList;
+
+
+    public SortedPersonList() {
+        hobbyList = new LList<Person>();
+        regionList = new LList<Person>();
+        majorList = new LList<Person>();
+    }
+
+
+    public void add(Person newEntry, String command) {
+        if (command == "hobby") {
+            hobbyList.add(newEntry);
+        }
+        else {
+            if (command == "major") {
+                majorList.add(newEntry);
+            }
+            else {
+                regionList.add(newEntry);
+            }
+        }
+    }
+}
