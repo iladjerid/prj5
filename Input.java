eeec28c18a64c45282add4c670aeb898ec1b145d
kayley25 <kayley25@vt.edu>
2017-04-19 18:58:53 -0400

    Update Input.java

--- Input.java
+++ Input.java
@@ -6,9 +6,33 @@ package prj5;
 
     public class Input {
 
-        /**
-        */
-        public static void main(String[] args) {
-        }
+    public static void main(String[] args) {
+        ListScanner scanner = new ListScanner(args[0], args[1]);
+        SortedPeopleList people = scanner.people;
+        int[] heardSports = scanner.heardSport;
+        int[] likeSports = scanner.likeSport;
+        int[] heardMusic = scanner.heardMusic;
+        int[] likeMusic = scanner.likeMusic;
+        int[] heardRead = scanner.heardRead;
+        int[] likeRead = scanner.likeRead;
+        int[] heardArt = scanner.heardArt;
+        int[] likeArt = scanner.likeArt;
+        LList<Song> genre = scanner.songs.getSongListGenre();
 
+        for (int i = 1; i <= genre.getLength(); i++) {
+            Song s = genre.getEntry(i);
+            System.out.println("Song Title: " + s.getName());
+            System.out.println("Song Artist: " + s.getArtist());
+            System.out.println("Song Genre: " + s.getGenre());
+            System.out.println("Song Year: " + s.getYear());
+            System.out.println("Heard");
+            System.out.println("reading:" + heardRead + " art:" + heardArt
+                + " sports:" + heardSports + " music:" + heardMusic);
+            System.out.println("Likes");
+            System.out.println("reading:" + likeRead + " art:" + likeArt
+                + " sports:" + likeSports + " music:" + likeMusic);
+        }
     }
+
+}
+
