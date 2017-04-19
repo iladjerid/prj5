31407ee7d247f54012d3004b092e90307c6173ec
kayley25 <kayley25@vt.edu>
2017-04-19 12:33:45 -0400

    Update ListScanner.java

--- ListScanner.java
+++ ListScanner.java
@@ -8,5 +8,47 @@ package prj5;
  *
  */
 public class ListScanner {
-    
+
+    public ListScanner(String fileNameSurvey, String fileNameSong) {
+
+        GUIDisplay disp = new GUIDisplay();
+    }
+
+
+    public SortedPersonList readListFileSurvey(String fileName) {
+        SortedPersonList list = new SortedPersonList();
+        File f = new File(fileName);
+        try {
+            Scanner file = new Scanner(f);
+            while(file.hasNextLine()) {
+                String data[] = file.nextLine().split("*,*");
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+
+    public SortedSongList readListFileSong(String fileName) {
+        SortedSongList list = new SortedSongList();
+        File f = new File(fileName);
+        try {
+            Scanner file = new Scanner(f);
+            while (file.hasNextLine()) {
+                String data[] = file.nextLine().split("*,*");
+                int year = Integer.parseInt(data[2]);
+                Song song = new Song(data[0], data[1], data[3], year);
+                list.addName(song);
+                list.addArtist(song);
+                list.addGenre(song);
+                list.addYear(song);
+            }
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
