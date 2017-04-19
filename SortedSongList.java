6d9ca8603f4b887a8dea8a98c2a943e2c8dd3551
kayley25 <kayley25@vt.edu>
2017-04-19 12:28:32 -0400

    Update SortedSongList.java

--- SortedSongList.java
+++ SortedSongList.java
@@ -1,36 +1,140 @@
 public class SortedSongList {
-    private LList<Song> songList;
+    private LList<Song> songListName;
+    private LList<Song> songListArtist;
+    private LList<Song> songListGenre;
+    private LList<Song> songListYear;
 
 
     public SortedSongList() {
-        songList = null;
+        songListName = new LList<Song>();
+        songListArtist = new LList<Song>();
+        songListGenre = new LList<Song>();
+        songListYear = new LList<Song>();
     }
 
 
-    public void add(Song newEntry, String command) {
+    public void addName(Song newEntry) {
         boolean added = false;
-        if (songList.isEmpty()) {
-            songList.add(newEntry);
+        if (songListName.isEmpty()) {
+            songListName.add(newEntry);
         }
         else {
-            for (int i = 1; i <= songList.getLength(); i++) {
-                if (songList.getEntry(i).compareTo(newEntry, command) == -1) {
-                    songList.add(i - 1, newEntry);
+            for (int i = 1; i <= songListName.getLength(); i++) {
+                if (songListName.getEntry(i).compareTo(newEntry,
+                    "name") == -1) {
+                    songListName.add(i - 1, newEntry);
                     added = true;
                     break;
                 }
             }
             if (!added) {
-                songList.add(newEntry);
+                songListName.add(newEntry);
             }
         }
     }
-    
-    public LList<Song> getSongList() {
-        return songList;
+
+
+    public void addArtist(Song newEntry) {
+        boolean added = false;
+        if (songListArtist.isEmpty()) {
+            songListArtist.add(newEntry);
+        }
+        else {
+            for (int i = 1; i <= songListArtist.getLength(); i++) {
+                if (songListArtist.getEntry(i).compareTo(newEntry,
+                    "artist") == -1) {
+                    songListArtist.add(i - 1, newEntry);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                songListArtist.add(newEntry);
+            }
+        }
     }
 
-    public int getSize() {
-        return songList.getLength();
+
+    public void addGenre(Song newEntry) {
+        boolean added = false;
+        if (songListGenre.isEmpty()) {
+            songListGenre.add(newEntry);
+        }
+        else {
+            for (int i = 1; i <= songListGenre.getLength(); i++) {
+                if (songListGenre.getEntry(i).compareTo(newEntry,
+                    "genre") == -1) {
+                    songListGenre.add(i - 1, newEntry);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                songListGenre.add(newEntry);
+            }
+        }
+
     }
+
+
+    public void addYear(Song newEntry) {
+        boolean added = false;
+        if (songListYear.isEmpty()) {
+            songListYear.add(newEntry);
+        }
+        else {
+            for (int i = 1; i <= songListYear.getLength(); i++) {
+                if (songListYear.getEntry(i).compareTo(newEntry,
+                    "year") == -1) {
+                    songListYear.add(i - 1, newEntry);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                songListYear.add(newEntry);
+            }
+        }
+    }
+
+
+    public LList<Song> getSongListName() {
+        return songListName;
+    }
+
+
+    public int getNameSize() {
+        return songListName.getLength();
+    }
+
+
+    public LList<Song> getSongListArtist() {
+        return songListArtist;
+    }
+
+
+    public int getArtistSize() {
+        return songListArtist.getLength();
+    }
+
+
+    public LList<Song> getSongListGenre() {
+        return songListGenre;
+    }
+
+
+    public int getGenreSize() {
+        return songListGenre.getLength();
+    }
+
+
+    public LList<Song> getSongListYear() {
+        return songListYear;
+    }
+
+
+    public int getYearSize() {
+        return songListYear.getLength();
+    }
+
 }
